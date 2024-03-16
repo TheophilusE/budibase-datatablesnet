e5d13b3dd303cd90ebed239cfdb5fe53be0067be
Theophilus Eriata <theoeriata@gmail.com>
2024-03-16 14:06:59 -0600

    Expose data table options.
    * Removes redundant information.

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,22 +1,31 @@
 <script>
   import { getContext } from "svelte"
   import Table from "../components/Table.svelte";
+
   export let search
   export let resultPage
   export let pagination
-  export let parSearchableColumns
   export let dataProvider
-  export let parRowsPerPage
-  export let parSortOrder
-  export let parSortBy
   export let columns
-  export let tableEvent
   export let searchableColumns
+  export let rowsPerPage
+  export let sortOrder
+  export let sortBy
+  export let tableEvent
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
 </script>
 
 <div use:styleable={$component.styles}>
-  <Table dataProvider={dataProvider} search={search} resultPage={resultPage} pagination={pagination} searchableColumns={searchableColumns} parRowsPerPage={parRowsPerPage} parSortOrder={parSortOrder} columns={columns} parSortBy={parSortBy} tableEvent={tableEvent} />
+  <Table dataProvider={dataProvider}
+    search={search}
+    resultPage={resultPage}
+    pagination={pagination}
+    columns={columns}
+    searchableColumns={searchableColumns}
+    rowsPerPage={rowsPerPage}
+    sortOrder={sortOrder}
+    sortBy={sortBy}
+    tableEvent={tableEvent} />
 </div>
